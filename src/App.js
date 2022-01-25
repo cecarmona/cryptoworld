8ccef1d6d9ee20b44829316eb909202dbafccccd
cecarmona <cecarmona24@gmail.com>
2022-01-25 03:21:52 -0400

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>Hi again C2 Coder!</div>
+    <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+
+        </div>
+        <div className="footer"></div>
+    </div>
   );
 };
 
